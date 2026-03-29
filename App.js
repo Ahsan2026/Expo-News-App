df8dc7d6ee4e977662047ee6205cfac241c2e08b
Ahsan Ahmed <ahsanahmedf2003@gmail.com>
2026-03-29 22:29:09 +0500

    Initial commit - News App with Navigation

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+ 
+
+import { NavigationContainer } from "@react-navigation/native";
+import StackNavigator from "./navigation/StackNavigator";
+import { BookmarkProvider } from "./context/BookmarkContext";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <BookmarkProvider>
+      <NavigationContainer>
+        <StackNavigator />
+      </NavigationContainer>
+    </BookmarkProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
+
+
+
+
+// import { NavigationContainer } from "@react-navigation/native";
+// import { BookmarkProvider } from "./context/BookmarkContext";
+// import BottomTabs from "./navigation/BottomTabs";
+
+// export default function App() {
+//   return (
+//     <BookmarkProvider>
+//       <NavigationContainer>
+//         <BottomTabs />
+//       </NavigationContainer>
+//     </BookmarkProvider>
+//   );
+// }
+
